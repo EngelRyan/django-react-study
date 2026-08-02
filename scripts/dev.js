4296a0728724a0377e33687eb43125b22b52a459
Bia-Reichert <biamcd@gmail.com>
2026-08-01 22:45:42 -0300

    Lovers | Script | Update dev script by removing unnecessary flags verification

--- scripts/dev.js
+++ scripts/dev.js
@@ -5,16 +5,6 @@ const run = (command) => {
   execSync(command, { stdio: "inherit" });
 };
 
-const args = process.argv;
-
-if (args.includes("--setup")) {
-  run("npm run setup");
-}
-
-if (args.includes("--m")) {
-  run("npm run migrations");
-}
-
 run(
   'concurrently --names "FRONTEND,BACKEND" --prefix-colors "blue,green" "npm run frontend" "npm run backend"'
 );
